afb695516c5a0f2e9696a42aaeb903bc979d7847
Eleyka <eleynekrd@gmail.com>
2018-03-20 18:52:04 -0500

    Componente Payment Dinámico

--- src/components/Payment-item.js
+++ src/components/Payment-item.js
@@ -1,16 +1,10 @@
 import React from 'react';
 
-const Paymentitem = () =>(
+const Paymentitem = ({logopayment}) =>(
   <div>
-    <a><img src="assets/img/logo/bcp.png" alt="bcp"/><br/>Agentes y Bodegas Agencias</a>
-    <a><img src="assets/img/logo/bbva.png" alt="bbva"/><br/>Agentes y Bodegas Agencias</a>
-    <a><img src="assets/img/logo/interbank.jpg" alt="interbank"/><br/>Agentes y Bodegas Agencias</a>
-    <a><img src="assets/img/logo/banbif.png" alt="banbif"/><br/>Agencias</a>
-    <a><img src="assets/img/logo/full-carga.png" alt="full"/><br/>Agentes y Bodegas</a>
-    <a><img src="assets/img/logo/Scotiabank.jpg" alt="Scotiabank"/><br/>Agentes y Bodegas Agencias</a>
-    <a><img src="assets/img/logo/western-union.png" alt="western"/><br/>Agentes y Bodegas</a>
-    <a><img src="assets/img/logo/Kasnet.png" alt="Kasnet"/><br/>Agentes y Bodegas</a>
-
+    {logopayment.map(e=>
+      <div key={e.name}{...e}><a href="#"><img src={e.link} title={e.name}/><br/>{e.text}</a></div>)
+  }
   </div>
 )
 export default Paymentitem
--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Paymentitem from './Payment-item.js'
 
+const logoPayment = [{name:'bcp', link:'assets/img/logo/bcp.png', text:'Agentes y Bodegas Agencias'},
+                     {name:'bbva',link:'assets/img/logo/bbva.png', text:'Agentes y Bodegas Agencias'},
+                     {name:'interbank',link:'assets/img/logo/interbank.jpg', text:'Agentes y Bodegas Agencias'},
+                     {name:'banbif',link:'assets/img/logo/banbif.png', text:'Agentes y Bodegas'},
+                     {name:'full-carga',link:'assets/img/logo/full-carga.png', text:'Agentes y Bodegas'},
+                     {name:'Scotiabanck',link:'assets/img/logo/Scotiabank.jpg', text:'Agentes y Bodegas Agencias'},
+                     {name:'western',link:'assets/img/logo/western-union.png', text:'Agentes y Bodegas'},
+                     {name:'Kasnet',link:'assets/img/logo/Kasnet.png', text:'Agentes y Bodegas'}]
+
 const Payment = () =>(
 <div>
-  <Paymentitem />
+  <Paymentitem logopayment={logoPayment}/>
 </div>
 )
 export default Payment
