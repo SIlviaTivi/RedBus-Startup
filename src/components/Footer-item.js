dc401f16b42991b691c286c395dc85e3a458c16d
Eleyka <eleynekrd@gmail.com>
2018-03-20 17:40:28 -0500

    Footer Dinámico

--- src/components/Footer-item.js
+++ src/components/Footer-item.js
@@ -4,9 +4,7 @@ const Footeritem = ({footertitle}) => (
   <footer>
   {footertitle.map(e=>
       <span key={e.name}{...e}>{e.name}<a href="#"><span>{e.email}</span><img src={e.link} className="sizeimg"/></a></span>)
-  
   }
-
   </footer>
 )
 export default Footeritem
