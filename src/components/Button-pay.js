cd9aa221173e133998480dd374d5c4bef114d75b
Eleyka <eleynekrd@gmail.com>
2018-03-20 19:14:29 -0500

    Componente Button-Pay

---
+++ src/components/Button-pay.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const ButtonPay = () =>(
+  <div>
+  <button>Encuentra tu punto de pago</button>
+  <button>Ir a banca por internet</button>
+  </div>
+)
+export default ButtonPay
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Operation from './Operation';
 import Payment from './Payment';
+import ButtonPay from './Button-pay';
 
 const Main =() =>(
     <div>
         <Operation/>
         <Payment/>
+        <ButtonPay/>
     </div>
 )
 export default Main
