9cbae3953757b7f03e4e76a7d6e07c2ca648e0e8
SIlviaTivi <skfl.erg@hotmail.com>
2018-03-20 19:45:35 -0500

    Componente Instruction dinamico

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Operation from './Operation';
 import Payment from './Payment';
+import Instruction from './Instruction'
 
 const Main =() =>(
     <div>
         <Operation/>
         <Payment/>
+        <Instruction/>
     </div>
 )
 export default Main
