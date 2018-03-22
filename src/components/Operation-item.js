1d8feba31fe37bc528f67f1a1f60735f49e85eba
SIlviaTivi <skfl.erg@hotmail.com>
2018-03-22 03:53:26 -0500

    solucionando conflicto

--- src/components/Operation-item.js
+++ src/components/Operation-item.js
@@ -1,12 +1,21 @@
 import React from 'react';
-
+<div></div>
 const OperationItem = ({operation}) => (
-  <div>
-  {operation.map(e=>
-      <span key={e.name}{...e}>
-      <img className='sizeimg' src={e.img} className="sizeimg"/>{e.name}<a href="#"><span>{e.code}</span><span>{e.total}</span></a>
-      </span>)
-  }
+  <div className='operation'>
+    <div className='operation-block'>
+      {operation.map(e=>
+      
+          <div key={e.name}{...e} className="block-operation">
+          <div className="blocktotal">
+            <img  className="clock" src={e.img} />
+            <div className = {e.class1}><div >{e.namecode}</div> <div className = "fdcode" >{e.code}</div></div>
+            <div className = {e.class2}><div className = "fdtotal" >{e.nametotal}</div><div>{e.total}</div></div>
+            <div className = {e.class3}><div className = "fdorder" >{e.nameday}</div><div className = "fdday" >{e.day}</div></div>
+          </div>
+          </div>)
+
+      }
+    </div>
   </div>
 )
 export default OperationItem
